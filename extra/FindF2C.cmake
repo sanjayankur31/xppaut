82f4991ba664d8b76afa690ffd1b4b83ebda174c
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2015-09-03 17:55:36 +0100

    Build part looks complete.

--- extra/FindF2C.cmake
+++ extra/FindF2C.cmake
@@ -1,5 +1,15 @@
 message(STATUS "Looking for F2C")
 
+find_path(F2C_INCLUDE_DIR
+    f2c.h
+    )
+
+if(F2C_INCLUDE_DIR)
+    message(STATUS "Found f2c include directory: ${F2C_INCLUDE_DIR}")
+else()
+    message(STATUS "Could not fund f2c include dir")
+endif()
+
 if(F2C_LIBRARIES)
     message(STATUS "Found f2c libraries ${F2C_LIBRARIES}")
     set(F2C_FOUND_LIBS TRUE)
@@ -11,8 +21,9 @@ else()
     endif()
 endif()
 
-if(F2C_FOUND_LIBS)
+if(F2C_INCLUDE_DIR AND F2C_FOUND_LIBS)
     set(F2C_FOUND TRUE)
 else()
+    set(F2C_FOUND FALSE)
     message(STATUS "F2C: Cound not find f2c. Try setting F2C env var.")
 endif()
